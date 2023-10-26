b114cad61d04e4faecfe9f8d49d80add5ef6c6e7
drquandary <jeffreyvadala@gmail.com>
2023-10-26 11:33:30 -0400

    Fixed relative import error in data_import.py by changing the import statement.

--- to/data_import.py
+++ to/data_import.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from path.to.database import connect_to_database
+from database import connect_to_database
 
 def import_data(file_path, table_name, database_name):
     cnx, cursor = connect_to_database(database_name)
